02f71197d8f8c60d0936a0648f5bd4992077398e
ToxSCin <renatoserrano.am@gmail.com>
2022-11-16 15:54:53 -0300

    test

---
+++ SqliteTest/SqliteTest/Helper/SQliteDataBaseHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SqliteTest.Model;
+namespace SqliteTest.Helper
+{
+    public class SQliteDataBaseHelper
+    {
+        public void insert(Produto p);
+    }
+}
